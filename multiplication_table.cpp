bd3abc8ec4e2b79083b4b69e17f6b43f20813064
jeffrey837291 <mars837291@gmail.com>
2021-09-02 19:30:10 +0800

    fourth upload

--- multiplication_table.cpp
+++ multiplication_table.cpp
@@ -1,27 +1,18 @@
 #include<stdio.h>
 
-void main()
-
+int main()
 {
 
-　int i,j,result;
+int i,j,result;
 
-　printf(" ");
+printf(" ");
 
-　for (i=1;i<10;i++)
-
-{ for(j=1;j<10;j++)
-
-{
-
-result=i*j;
-
-printf("%d*%d=%-3d",i,j,result); /*-3d表示左對齊，佔3位*/
-
-}
-
+for (i=1;i<10;i++){ 
+   for(j=1;j<10;j++){
+      result=i*j;
+      printf("%d*%d=%-3d",i,j,result); /*-3d表示左對齊，佔3位*/
+   }
 printf(" "); /*每一行後換行*/
-
 }
 
 }
